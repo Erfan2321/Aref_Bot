6bd958c6030b589e61b960d4cb970dfbc913843e
erfan <erfansedaghat2321@gmail.com>
2025-09-15 23:01:42 +0330

    Delete src directory

--- src/main/java/org/example/TelegramData.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class TelegramData {
-
-    public static final String BOT_NAME = System.getenv("BOT_USERNAME");
-    public static final String BOT_TOKEN = System.getenv("BOT_API_TOKEN");
-
-    private TelegramData() {}
-}
